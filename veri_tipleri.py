2980c287a85d3dd72761005fda1c958396a056a2
Bekir GULER <90568365+bekirglr@users.noreply.github.com>
2021-10-02 21:29:27 +0300

    veri tipleri örnekleri

---
+++ veri_tipleri.py
@@ -0,0 +1,45 @@
+  
+  #integres = int
+  
+  #Integer'ler küsuratı olmayan, 1,2,3,4 gibi sayılardır.
+  #Integer'ler negatif değerler de alabilir 
+  #Nasıl tam sayılarda aritmetik işlemler yapıyorsak integerlarla da yapabiliriz.
+  #Nasıl tam sayılarda aritmetik işlemler yapıyorsak integerlarla da yapabiliriz
+
+ #print(type(5))
+
+ 
+
+#_________________________________________________
+
+
+   #float = float
+
+
+  #float'lar küsuratı olan, virgülden sonra değerler barındıran veri tipleridir. 1.3,2.4,3.3,4.6 gibi sayılardır.
+
+ #print(type(5.1))
+ 
+
+
+#_________________________________________________
+
+
+  #boolean
+
+  #Daha ileriki konularda if-else mantığını gördüğümüzde daha iyi oturacak bir veri tipi. Özetle bir şeyin doğru (True) veya yanlış (False) olduğunu belirten bir yapı
+
+ 
+
+#
+
+
+  #type casting = casting
+
+  #Data objelerinin tipini değiştirebiliyoruz, buna casting deniyor
+  #Burada olan işlem sayıyı yuvarlama işlemi değil, sadece tam kısmını alıyoruz!!!
+  
+ 
+ 
+#print(int(0.1)) 
+ 
